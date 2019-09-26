701d5f883f435c7eb926c17fa340707bc0be1848
Sunny Jain <sunnyjainfbd@gmail.com>
2019-09-26 23:45:14 +0530

    [ADDED]  const vals

--- app/src/main/java/com/example/nasapod/utils/Constants.kt
+++ app/src/main/java/com/example/nasapod/utils/Constants.kt
@@ -4,5 +4,7 @@ import com.example.nasapod.BuildConfig
 
 object Constants {
 
-    val API_URL = BuildConfig.BASE_URL
+    const val API_URL = BuildConfig.BASE_URL
+    const val API_KEY = "EVKE5uRkDPRdHIZwSgirofMweG2MaB6YMuWBgGtn"
+    const val PREFS = "ApodPrefs"
 }
