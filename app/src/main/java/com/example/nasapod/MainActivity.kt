a7a640bbaa7ccda357c1e8bf5feaa1143b04660c
sunnyjain <sunnyjainfbd@gmail.com>
2019-09-27 14:05:54 +0530

    [FIX] finish the activity when backpressed from the list page.

--- app/src/main/java/com/example/nasapod/MainActivity.kt
+++ app/src/main/java/com/example/nasapod/MainActivity.kt
@@ -59,6 +59,19 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector, Navigation
         }
     }
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        if (supportFragmentManager.backStackEntryCount <= 1) {
+            finish()
+        }
+
+        if(supportFragmentManager.backStackEntryCount > 1) {
+            tag = supportFragmentManager.getBackStackEntryAt(supportFragmentManager.backStackEntryCount - 1).name
+            currentVisibleFragment = supportFragmentManager.findFragmentByTag(tag)
+        }
+    }
+
     override fun onSelect(id: Int, bundle: Bundle) {
         if(id == 0) {
             //list view.
