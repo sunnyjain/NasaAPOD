67bec72577994ff7542ab08c9bacfd1b3d479a30
sunnyjain <sunnyjainfbd@gmail.com>
2019-09-27 14:04:08 +0530

    minor fixes. base url changed, added the '/' at the end. added adapter-rxjava2 retrofit lib. fixed the serializedname of few fields and assigned default values. added a nullable annotation to hdurl.

--- app/src/main/java/com/example/nasapod/commons/data/local/APODObject.kt
+++ app/src/main/java/com/example/nasapod/commons/data/local/APODObject.kt
@@ -3,16 +3,17 @@ package com.example.nasapod.commons.data.local
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import javax.annotation.Nullable
 
 
 @Entity
 data class APODObject(
 
     @SerializedName("date") @PrimaryKey val date: String,
-    @SerializedName("explanation") var explanation: String,
-    @SerializedName("hdurl") var hdurl: String,
-    @SerializedName("mediaType") var mediaType: String,
-    @SerializedName("serviceVersion") var serviceVersion: String,
-    @SerializedName("title") var title: String,
-    @SerializedName("url") var tileImageUrl: String
+    @SerializedName("explanation") var explanation: String = "",
+    @Nullable @SerializedName("hdurl") var hdurl: String?,
+    @SerializedName("media_type") var mediaType: String = "",
+    @SerializedName("service_version") var serviceVersion: String = "",
+    @SerializedName("title") var title: String = "",
+    @SerializedName("url") var tileImageUrl: String = ""
 )
