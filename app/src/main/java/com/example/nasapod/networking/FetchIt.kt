4d0237caea0fa827d7a8e0798babdedd8762f125
sunnyjain <sunnyjainfbd@gmail.com>
2019-09-27 14:21:39 +0530

    [UPDATED] fetchit class to correctly compare the date and [ADDED] udpateLastFetchDate func.

--- app/src/main/java/com/example/nasapod/networking/FetchIt.kt
+++ app/src/main/java/com/example/nasapod/networking/FetchIt.kt
@@ -2,6 +2,7 @@ package com.example.nasapod.networking
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.util.Log
 import com.example.nasapod.networking.FetchItKeys.LAST_FETCH_DATE
 import com.example.nasapod.utils.Constants.PREFS
 import java.text.SimpleDateFormat
@@ -29,37 +30,50 @@ object FetchIt {
 
         val currentDate = preferences?.getString(LAST_FETCH_DATE, "")
 
+        val todaysDate = Calendar.getInstance()
+        todaysDate.set(Calendar.HOUR_OF_DAY, 0)
+        todaysDate.set(Calendar.MINUTE, 0)
+        todaysDate.set(Calendar.SECOND, 0)
+        todaysDate.set(Calendar.MILLISECOND, 0)
+        todaysDate.add(Calendar.DAY_OF_YEAR, -1)
+
         //if there is not record of current date is found
         if(currentDate.isNullOrEmpty()) {
             //fetch records, that is current_date + 20 days.
-            return  fetchIt()
+            return  fetchIt(todaysDate.time)
         }
 
         //if todays date is larger than current date.
         val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-        if(Date() > sdf.parse(currentDate)) {
-            return fetchIt()
+
+        //so it will always be one day before not on the same day.
+
+        if(todaysDate.time > sdf.parse(currentDate)) {
+            return FETCH_IT
         }
 
         return DONT_FETCH_IT
     }
 
+    fun updateLastFetchDate(date: Date) {
+        saveCurrenDate(date)
+    }
 
-    private fun fetchIt(): Boolean {
-        saveCurrentTime()
+    private fun fetchIt(date: Date): Boolean {
+        saveCurrenDate(date)
         return FETCH_IT
     }
 
-    private fun saveCurrentTime() {
+    private fun saveCurrenDate(date: Date) {
         preferences
             ?.edit()
             ?.putString(LAST_FETCH_DATE,
-                (SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())).format(Date()))
+                (SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())).format(date))
             ?.apply()
     }
 
     private fun performPrefsSanityCheck() {
         if (preferences == null)
-            throw IllegalStateException("Make sure to init Synk")
+            throw IllegalStateException("Make sure to init FetchIt")
     }
 }
