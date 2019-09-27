da509eaf6638724e6dc9893bf229a9bd4b17d877
sunnyjain <sunnyjainfbd@gmail.com>
2019-09-27 14:11:02 +0530

    [UPDATED] added necessary methods for database transactions.

--- app/src/main/java/com/example/nasapod/commons/data/local/ApodDao.kt
+++ app/src/main/java/com/example/nasapod/commons/data/local/ApodDao.kt
@@ -1,8 +1,21 @@
 package com.example.nasapod.commons.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import io.reactivex.Flowable
+import io.reactivex.Single
 
 @Dao
 interface ApodDao {
 
+    @Query("SELECT * FROM APODObject ORDER BY Date(date) DESC")
+    fun getAPODList(): Flowable<List<APODObject>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun saveAPODs(apods: List<APODObject>)
+
+    @Query("SELECT date FROM APODObject ORDER BY Date(date) ASC LIMIT 1")
+    fun getMinDateAvailable(): Single<String>
 }
