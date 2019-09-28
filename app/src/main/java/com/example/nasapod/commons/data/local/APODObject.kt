6d64921e80532ecf7588545e7acdecd67ddc469e
sunnyjain <sunnyjainfbd@gmail.com>
2019-09-28 12:47:25 +0530

    [ADDED] ids for pagination in APODObject. updated dao file.

--- app/src/main/java/com/example/nasapod/commons/data/local/APODObject.kt
+++ app/src/main/java/com/example/nasapod/commons/data/local/APODObject.kt
@@ -1,15 +1,19 @@
 package com.example.nasapod.commons.data.local
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import javax.annotation.Nullable
 
 
-@Entity
+@Entity(indices = [Index(value = ["date"], unique = true)])
 data class APODObject(
 
-    @SerializedName("date") @PrimaryKey val date: String,
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    @ColumnInfo(name = "date")
+    @SerializedName("date") val date: String,
     @SerializedName("explanation") var explanation: String = "",
     @Nullable @SerializedName("hdurl") var hdurl: String?,
     @SerializedName("media_type") var mediaType: String = "",
--- app/src/main/java/com/example/nasapod/commons/data/local/ApodDao.kt
+++ app/src/main/java/com/example/nasapod/commons/data/local/ApodDao.kt
@@ -10,12 +10,16 @@ import io.reactivex.Single
 @Dao
 interface ApodDao {
 
-    @Query("SELECT * FROM APODObject ORDER BY Date(date) DESC")
-    fun getAPODList(): Flowable<List<APODObject>>
+    /*this will limit the query to return 20 records from the last offset limit.*/
+    @Query("SELECT * FROM APODObject where id >= :startIndex and id < :endIndex ORDER BY Date(date) DESC")
+    fun getAPODList(startIndex: Long, endIndex: Long): Flowable<List<APODObject>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun saveAPODs(apods: List<APODObject>)
 
     @Query("SELECT date FROM APODObject ORDER BY Date(date) ASC LIMIT 1")
     fun getMinDateAvailable(): Single<String>
+
+    @Query("SELECT id FROM APODObject ORDER BY id DESC LIMIT 1")
+    fun getLastRecordId(): Single<Long>
 }
