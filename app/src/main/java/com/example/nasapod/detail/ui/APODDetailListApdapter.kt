97c5311bb3582a0ce15fa616d255a3ab6e20eb19
sunnyjain <sunnyjainfbd@gmail.com>
2019-09-29 20:30:57 +0530

    [ADDED] detail information in the detail view.

--- app/src/main/java/com/example/nasapod/detail/ui/APODDetailListApdapter.kt
+++ app/src/main/java/com/example/nasapod/detail/ui/APODDetailListApdapter.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nasapod.R
 
@@ -34,10 +35,13 @@ class APODDetailListApdapter @Inject constructor(private val picasso: Picasso, p
     }
 
 
-    inner class APODItemViewHolder(itemView: View, zoomListener: ZoomEngine.Listener?): RecyclerView.ViewHolder(itemView) {
+    inner class APODItemViewHolder(itemView: View, zoomListener: ZoomEngine.Listener?): RecyclerView.ViewHolder(itemView), View.OnClickListener {
+       
 
         init {
             itemView.apod_img.engine.addListener(zoomListener!!)
+            itemView.apod_detail_btn.setOnClickListener(this)
+            itemView.apod_detail_btn.tag = "ImageView"
         }
 
         fun bind(item: APODObject, picasso: Picasso) {
@@ -58,7 +62,32 @@ class APODDetailListApdapter @Inject constructor(private val picasso: Picasso, p
 
                 })
 
-            itemView.apod_title.text = item.title
+            itemView.title.text = item.title
+            itemView.date.text = item.date
+            if(item.explanation.isEmpty())
+                itemView.lbl_explanantion.visibility = View.GONE
+            else {
+                itemView.lbl_explanantion.visibility = View.VISIBLE
+                itemView.lbl_explanantion.text = itemView.context.getString(R.string.explanation, item.explanation)
+
+            }
+
         }
+
+        override fun onClick(p0: View?) {
+            if(p0?.id == R.id.apod_detail_btn) {
+                if(p0.tag == "ImageView") {
+                    //show details.
+                    itemView.details_view.visibility = View.VISIBLE
+                    (p0 as TextView).text = itemView.context.getString(R.string.btn_lbl_close)
+                    itemView.apod_detail_btn.tag = "DetailsView"
+                } else {
+                    itemView.details_view.visibility = View.GONE
+                    (p0 as TextView).text = itemView.context.getString(R.string.lbl_details_view)
+                    itemView.apod_detail_btn.tag = "ImageView"
+                }
+            }
+        }
+            
     }
 }
