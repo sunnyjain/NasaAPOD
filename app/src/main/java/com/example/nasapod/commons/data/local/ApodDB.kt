a40d8628c0e783def7a0fc065ffe43a125014df0
sunnyjain <sunnyjainfbd@gmail.com>
2019-09-29 20:30:30 +0530

    schema version changed.

--- app/src/main/java/com/example/nasapod/commons/data/local/ApodDB.kt
+++ app/src/main/java/com/example/nasapod/commons/data/local/ApodDB.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 
-@Database(entities = [APODObject::class], version = 3, exportSchema =  false)
+@Database(entities = [APODObject::class], version = 5, exportSchema =  false)
 abstract class ApodDB : RoomDatabase() {
     abstract fun apodDao(): ApodDao
 }
